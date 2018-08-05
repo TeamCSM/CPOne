e4c4393010b79058a46146487e9b3069ca8ef037
Administrator <Administrator@Astory>
2018-08-05 20:07:38 +0900

    0805 / commit - pom.xml - servlet 버전 3.1 jsp api 버전 2.3.1 변경
    context 폴더 추가
    application-context.xml 추가
    dispathcerServlet-context.xml 변경
    
    view 페이지 추가
    main
    mail
    resources 폴더 추가
    main
    mail

---
+++ CPOne/src/main/java/kr/cpone/front/mail/controller/MailController.java
@@ -0,0 +1,15 @@
+package kr.cpone.front.mail.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MailController {
+
+	@RequestMapping(value="/cpone/front/mail/mailMainView.do")
+	public String mailMainView() throws Exception {
+		String mailMainView = "mail/mailMain";
+		
+		return mailMainView;
+	}
+}
